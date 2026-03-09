8ae129d6f9cc08b357251fcec9b92cf155f39f42
Samir <gabrielsamir525@gmail.com>
2026-03-09 13:25:17 -0600

    Add extraction of information with the tool csv

--- bot_frases.py
+++ bot_frases.py
@@ -2,14 +2,18 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 import time
+import csv
 navegador =webdriver.Firefox()
 navegador.get("http://quotes.toscrape.com/")
 time.sleep(2)
-lista_tarjetas = navegador.find_elements(By.CLASS_NAME,"quote")
-for tarjeta in lista_tarjetas:
-    frases= tarjeta.find_element(By.CLASS_NAME,"text")
-    autor= tarjeta.find_element(By.CLASS_NAME,"author")
-    print(f"{frases.text}--- Author {autor.text}")
-    print("")
+
+with open ('datos_recuperados.csv', mode = 'w',encoding ='utf-8') as document:
+    escritor = csv.writer(document)
+    lista_tarjetas = navegador.find_elements(By.CLASS_NAME,"quote")
+    for tarjeta in lista_tarjetas:
+        frase = tarjeta.find_element(By.CLASS_NAME,"text")
+        autor = tarjeta.find_element(By.CLASS_NAME,"author")
+
+        escritor.writerow([frase.text, autor.text])
 
 navegador.quit()
